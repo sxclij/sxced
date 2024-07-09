e84e8da679a6c6d832483e3e5e11a980016d3c86
sxclij <sxclij@gmail.com>
2024-07-09 00:35:08 +0000

    add global

--- src/main.c
+++ src/main.c
@@ -46,42 +46,40 @@ struct node {
     struct node* prev;
     char ch;
 };
+enum mode {
+    mode_insert,
+    mode_normal,
+};
 
-static struct node nodes[nodes_capacity];
-static struct node* nodes_passive[nodes_capacity];
-static struct node* nodes_target;
-uint32_t nodes_passive_size = nodes_capacity;
+static struct global {
+    struct node nodes[nodes_capacity];
+    struct node* nodes_passive[nodes_capacity];
+    struct node* nodes_target;
+    uint32_t nodes_passive_size;
+    enum mode mode;
+} g;
 
 void node_insert(char ch) {
-    struct node* this = nodes_passive[--nodes_passive_size];
-    struct node* next = nodes_target;
-    struct node* prev = nodes_target->prev;
+    struct node* this = g.nodes_passive[--g.nodes_passive_size];
+    struct node* next = g.nodes_target;
+    struct node* prev = g.nodes_target->prev;
     this->ch = ch;
-    this->next = nodes_target;
-    this->prev = nodes_target->prev;
+    this->next = g.nodes_target;
+    this->prev = g.nodes_target->prev;
     next->prev = this;
     if (prev != NULL)
         prev->next = this;
 }
 void node_delete() {
-    if (nodes_target->prev == NULL)
+    if (g.nodes_target->prev == NULL)
         return;
-    struct node* this = nodes_target->prev;
+    struct node* this = g.nodes_target->prev;
     struct node* next = this->next;
     struct node* prev = this->prev;
-    nodes_passive[nodes_passive_size++] = this;
+    g.nodes_passive[g.nodes_passive_size++] = this;
     next->prev = prev;
     if (prev != NULL)
         prev->next = next;
-}
-
-void init() {
-    term_init();
-    printf("\e[>5h");
-
-    for (uint32_t i = 0; i < nodes_capacity; i++)
-        nodes_passive[i] = &nodes[i];
-    nodes_target = &nodes[0];
 }
 
 void update_rendering_clear() {
@@ -95,11 +93,11 @@ void update_rendering_clear() {
 }
 void update_rendering() {
     update_rendering_clear();
-    struct node* this = nodes_target;
+    struct node* this = g.nodes_target;
     while (this->prev != NULL)
         this = this->prev;
     while (this != NULL) {
-        if (this == nodes_target)
+        if (this == g.nodes_target)
             putchar('|');
         putchar(this->ch);
         this = this->next;
@@ -117,11 +115,11 @@ void update_input() {
                 if (ch == '[') {
                     ch = input[++i];
                     if (ch == 'D')
-                        if (nodes_target->prev != NULL)
-                            nodes_target = nodes_target->prev;
+                        if (g.nodes_target->prev != NULL)
+                            g.nodes_target = g.nodes_target->prev;
                     if (ch == 'C')
-                        if (nodes_target->next != NULL)
-                            nodes_target = nodes_target->next;
+                        if (g.nodes_target->next != NULL)
+                            g.nodes_target = g.nodes_target->next;
                 }
                 break;
             case '\b':
@@ -138,6 +136,16 @@ void update() {
     update_input();
     update_rendering();
     usleep(1000);
+}
+void init() {
+    term_init();
+    printf("\e[>5h");
+
+    g.nodes_passive_size = nodes_capacity;
+    for (uint32_t i = 0; i < nodes_capacity; i++)
+        g.nodes_passive[i] = &g.nodes[i];
+    g.nodes_target = &g.nodes[0];
+    g.mode = mode_insert;
 }
 
 int main() {
