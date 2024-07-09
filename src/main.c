10328a53312258e43a91ba7f42b3705b71e130aa
sxclij <sxclij@gmail.com>
2024-07-09 01:19:44 +0000

    add mode

--- src/main.c
+++ src/main.c
@@ -57,6 +57,7 @@ static struct global {
     struct node* nodes_target;
     uint32_t nodes_passive_size;
     enum mode mode;
+    uint32_t input_str4;
 } g;
 
 void node_insert(char ch) {
@@ -67,74 +68,125 @@ void node_insert(char ch) {
     this->next = g.nodes_target;
     this->prev = g.nodes_target->prev;
     next->prev = this;
-    if (prev != NULL)
+    if (prev != NULL) {
         prev->next = this;
+    }
 }
 void node_delete() {
-    if (g.nodes_target->prev == NULL)
+    if (g.nodes_target->prev == NULL) {
         return;
+    }
     struct node* this = g.nodes_target->prev;
     struct node* next = this->next;
     struct node* prev = this->prev;
     g.nodes_passive[g.nodes_passive_size++] = this;
     next->prev = prev;
-    if (prev != NULL)
+    if (prev != NULL) {
         prev->next = next;
+    }
 }
 
-void update_rendering_clear() {
+void input_normal(uint32_t str) {
+    uint32_t a = str % 65536;
+    switch (str % 256) {
+        case 'h':
+            if (g.nodes_target->prev != NULL) {
+                g.nodes_target = g.nodes_target->prev;
+            }
+            break;
+        case 'l':
+            if (g.nodes_target->next != NULL) {
+                g.nodes_target = g.nodes_target->next;
+            }
+            break;
+        case 'G':
+            while (g.nodes_target->next != NULL) {
+                g.nodes_target = g.nodes_target->next;
+            }
+            break;
+        case 'i':
+            g.mode = mode_insert;
+            break;
+        case 'q':
+            exit(0);
+        default:
+            break;
+    }
+    switch (str % 65536) {
+        case 26471:  // gg
+            while (g.nodes_target->prev != NULL) {
+                g.nodes_target = g.nodes_target->prev;
+            }
+            break;
+        default:
+            break;
+    }
+}
+void input_insert(uint32_t str) {
+    switch (str % 256) {
+        case 27:
+            g.mode = mode_normal;
+            break;
+        case '\b':
+        case 127:
+            node_delete();
+            break;
+        default:
+            node_insert(str);
+            break;
+    }
+}
+void input(uint32_t str) {
+    if (g.mode == mode_insert) {
+        input_insert(str);
+    } else if (g.mode == mode_normal) {
+        input_normal(str);
+    }
+}
+void input_readterm() {
+    char term_buf[term_capacity];
+    uint32_t input_size = term_read(term_buf);
+    for (uint32_t i = 0; i < input_size; i++) {
+        g.input_str4 <<= 8;
+        g.input_str4 += term_buf[i];
+        input(g.input_str4);
+    }
+}
+
+void rendering_clear() {
     printf("\e[1;1H");
     for (uint32_t i = 0; i < 10; i++) {
-        for (uint32_t j = 0; j < 64; j++)
+        for (uint32_t j = 0; j < 64; j++) {
             putchar(' ');
+        }
         putchar('\n');
     }
     printf("\e[1;1H");
 }
-void update_rendering() {
-    update_rendering_clear();
+void rendering() {
+    rendering_clear();
+    if (g.mode == mode_insert) {
+        printf("---[mode:insert]---\n");
+    } else if (g.mode == mode_normal) {
+        printf("---[mode:normal]---\n");
+    }
     struct node* this = g.nodes_target;
-    while (this->prev != NULL)
+    while (this->prev != NULL) {
         this = this->prev;
+    }
     while (this != NULL) {
-        if (this == g.nodes_target)
+        if (this == g.nodes_target) {
             putchar('|');
+        }
         putchar(this->ch);
         this = this->next;
     }
     fflush(stdout);
 }
-void update_input() {
-    char input[term_capacity];
-    uint32_t input_size = term_read(input);
-    for (uint32_t i = 0; i < input_size; i++) {
-        char ch = input[i];
-        switch (ch) {
-            case 27:
-                ch = input[++i];
-                if (ch == '[') {
-                    ch = input[++i];
-                    if (ch == 'D')
-                        if (g.nodes_target->prev != NULL)
-                            g.nodes_target = g.nodes_target->prev;
-                    if (ch == 'C')
-                        if (g.nodes_target->next != NULL)
-                            g.nodes_target = g.nodes_target->next;
-                }
-                break;
-            case '\b':
-            case 127:
-                node_delete();
-                break;
-            default:
-                node_insert(ch);
-                break;
-        }
-    }
-}
+
 void update() {
-    update_input();
-    update_rendering();
+    input_readterm();
+    rendering();
     usleep(1000);
 }
 void init() {
@@ -142,8 +194,9 @@ void init() {
     printf("\e[>5h");
 
     g.nodes_passive_size = nodes_capacity;
-    for (uint32_t i = 0; i < nodes_capacity; i++)
+    for (uint32_t i = 0; i < nodes_capacity; i++) {
         g.nodes_passive[i] = &g.nodes[i];
+    }
     g.nodes_target = &g.nodes[0];
     g.mode = mode_insert;
 }
